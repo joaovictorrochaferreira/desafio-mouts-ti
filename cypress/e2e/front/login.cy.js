82ef50e937c8c7a8d2ba87055d97c09f73d7d668
Joao Victor Rocha Ferreira <victorjoao1996@gmail.com>
2025-08-27 22:23:06 -0300

    fix<front/login.cy.js>:user login fix

--- cypress/e2e/front/login.cy.js
+++ cypress/e2e/front/login.cy.js
@@ -1,19 +1,15 @@
 /// <reference types="cypress"/>
 
 describe("User login", function () {
-  let userData;
-
-  before(() => {
-    cy.fixture("user.json").then((data) => {
-      userData = data;
-    });
-  });
-
   it("Login", function () {
-    cy.visit("/login");
-    cy.get('[data-testid="email"]').type(userData.email);
-    cy.get('[data-testid="senha"]').type(userData.password);
-    cy.get('[data-testid="entrar"]').click();
-    cy.contains("Serverest Store");
+    cy.getUsers().then((response) => {
+      const userLoginEmail = response.body.usuarios[0].email;
+      const userLoginPassword = response.body.usuarios[0].password;
+      cy.visit("/login");
+      cy.get('[data-testid="email"]').type(userLoginEmail);
+      cy.get('[data-testid="senha"]').type(userLoginPassword);
+      cy.get('[data-testid="entrar"]').click();
+      cy.contains("Serverest Store");
+    });
   });
 });
